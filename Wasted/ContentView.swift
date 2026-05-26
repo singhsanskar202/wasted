1ce4e280f1694dd7fbb9f2a648360412bb21b6bb
Sanskar <singhsanskar2000@gmail.com>
2026-05-26 18:35:52 +0530

    Initial Commit

---
+++ Wasted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wasted
+//
+//  Created by PapaJi on 26/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wasted/WastedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WastedApp.swift
+//  Wasted
+//
+//  Created by PapaJi on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WastedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
